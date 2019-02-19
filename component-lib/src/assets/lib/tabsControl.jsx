6f534c99bde19c9824fc162944c3ba1ae3e9e59b
princed <1138322908@qq.com>
2019-02-19 14:18:38 +0800

    1

---
+++ component-lib/src/assets/lib/tabsControl.jsx
@@ -0,0 +1,82 @@
+import React, { PureComponent } from 'react';
+import Swiper from 'swiper';
+
+class TabsControl extends PureComponent {
+    constructor(props) {
+        super(props);
+        this.initChildBullet();
+    }
+    initChildBullet() {
+        this.bullet = []
+        let { children = [] } = this.props;
+        return children.map((child, index) => {
+            const { label = "", value = index } = child.props;
+            this.bullet.push({ label, value });
+        })
+    }
+    defConf = {
+        freeMode: false,
+        bulletClass: "bullet",
+        slidesPerView: 1,
+        slideNextClass: "next",
+        slidePrevClass: "prev",
+        slideActiveClass: "activity",
+        bulletActiveClass: "activity",
+        paginationCurrentClass: "current",
+        paginationClickable: true,
+        paginationBulletRender: (swiper, index, className) => {
+            if (swiper.slides.length > 1) {
+                return `<li class="${className}">${this.bullet[index].label}</li>`;
+            } else {
+                return "";
+            }
+        },
+        onSlideChangeEnd: (swiper) => {
+            const { historyKey } = this.props;
+            if (historyKey) {   //如果有设置historyKey则保存slide切换状态
+                history.replaceState(this.merge(
+                    this.getHistoryState(),
+                    { [historyKey]: swiper.activeIndex }
+                ), null, location.pathname);
+            }
+        }
+    }
+    getHistoryState() {
+        return history.state || {};
+    }
+    merge(...rest) {
+        return rest.reduce((object, current) => {
+            return Object.assign(object, current)
+        }, {});
+    }
+    componentDidMount() {
+        const { conf, historyKey = "tabs" } = this.props;
+        const { [historyKey]: initialSlide = 0 } = this.getHistoryState();    //获取history堆栈中state用以定位到指定slide
+        const swiper = new Swiper(
+            this.refs.container,
+            this.merge(this.defConf, {          //合并conf参数
+                initialSlide,
+                pagination: this.refs.tabs,
+            }, conf)
+        );
+    }
+    render() {
+        const { className = "" } = this.props;
+        return (
+            <div className={`${className} list-swiper-subassembly`}>
+                <div className="swiper-tab">
+                    <ul className="clearfix" ref="tabs">
+
+                    </ul>
+                </div>
+                <div className="swiper-container" ref="container">
+                    <div className="swiper-wrapper clearfinx">
+                        {this.props.children}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default TabsControl;
--- component-lib/src/views/tab.jsx
+++ component-lib/src/views/tab.jsx
@@ -1,34 +1,34 @@
 import React, { Component } from 'react';
-import {Link} from "react-router-dom";
-import Swiper from 'swiper';
-// import '@css/tab.scss';
-import a from '@css/tab.less';
-console.log(a)
-class tab extends Component {
+import TabsControl from '@lib/tabsControl';
+import '@css/tab.less';
+
+class Tab extends Component {
     componentDidMount(){
-        var mySwiper = new Swiper('.swiper-container', {
-        
-        });
+       
     }
     render() {
         return (
             <div>
-                <Link to="/index" className="link">home</Link>
                 <div className="color">color</div>
-                <div className="swiper-container">
-                    <div className="swiper-wrapper">
-                        <div className="swiper-slide">Slide 1</div>
-                        <div className="swiper-slide">Slide 2</div>
-                        <div className="swiper-slide">Slide 3</div>
+                <div className="">
+                <TabsControl className="demo-swiper" historyKey="ptype">
+                    <div className="swiper-slide slide1" label="出借计划"> 
+                        <span>silde 1</span>
                     </div>
-                    <div className="swiper-pagination"></div>
-                    <div className="swiper-button-prev"></div>
-                    <div className="swiper-button-next"></div>
-                    <div className="swiper-scrollbar"></div>
-                </div>
+                    <div className="swiper-slide slide2" label="散标列表"> 
+                        <span> silde 2 </span> 
+                        <TabsControl className="second-swiper" conf={{ nested : true }} historyKey="ctype">
+                            <div className="swiper-slide slide5" label="看看"> silde 15 </div>
+                            <div className="swiper-slide slide6" label="笑笑"> silde 16 </div>
+                            <div className="swiper-slide slide7" label="哭哭"> silde 17 </div>
+                        </TabsControl>
+                    </div>
+                    <div className="swiper-slide slide3" label="转让专区"> silde 3 </div>
+                </TabsControl>
+            </div>
             </div>
         );
     }
 }
 
-export default tab;
+export default Tab;
